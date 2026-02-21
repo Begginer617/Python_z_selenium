e63949e250b81858e6341ebd70559e8946394a60
Karol Kolański <kkolanski.edu@gmail.com>
2026-02-21 15:42:41 +0100

    Click gender

--- main.py
+++ main.py
@@ -4,8 +4,14 @@ from selenium.webdriver import Chrome
 from time import sleep
 from selenium.webdriver.common.by import By
 
+
+class Gender:
+    MALE = 0
+    FEMALE = 1
+
 # Dane testowe
 DATA_EMAIL = "kdhfsjdh@spam.la"
+DATA_GENDER = Gender.MALE
 
 # Stworzenie instancji klasy Chrome
 # (to otworzy przeglądarkę)
@@ -22,6 +28,17 @@ sign_in_a.click()
 
 # Wpisz email
 driver.find_element(By.ID, "email_create").send_keys(DATA_EMAIL)
+
+# Kliknij Create Account
+driver.find_element(By.ID, "SubmitCreate").click()
+
+driver.implicitly_wait(5)
+# Kliknij swoją płeć
+if DATA_GENDER == Gender.FEMALE:
+    driver.find_element(By.XPATH, '//label[@for="id_gender2"]').click()
+
+else:
+    driver.find_element(By.XPATH, '//label[@for="id_gender1"]').click()
 
 sleep(5)
 driver.quit()
